01fbf9ba71590ee443bdfc3eb152a3b47e0533d1
theine <divineebube@gmail.com>
2022-09-08 04:08:46 +0100

    Added all my C# scripts  for project to repo

---
+++ Scripts/CameraFollow.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Transform player;
+
+    private Vector3 temPos;
+    [SerializeField] private float minX, maxX;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        
+    }
+ 
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (!player)
+            return;
+        temPos = transform.position;
+        temPos.x = player.position.x;
+
+        if (temPos.x > maxX)
+            temPos.x = maxX;
+        if (temPos.x < minX)
+            temPos.x = minX;
+
+        transform.position = temPos;
+    }
+}
---
+++ Scripts/Personal.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+
+
+public class Personal : MonoBehaviour
+{
+
+
+    
+
+    void Start()
+    {
+        
+
+   
+
+    }
+
+
+
+   
+    void Update()
+    {
+        
+    }
+}
